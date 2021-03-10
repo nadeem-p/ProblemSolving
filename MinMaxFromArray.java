e69d5a2092c296f4d8f687c6c4d0ffd8ffc728b3
Nadeem Patwekar <pnadeem47@gmail.com>
2021-03-10 22:51:47 +0530

    initial commit

---
+++ MinMaxFromArray.java
@@ -0,0 +1,9 @@
+/**
+ * MinMaxFromArray : https://www.geeksforgeeks.org/maximum-and-minimum-in-an-array/
+ */
+public class MinMaxFromArray {
+    public static void main(String[] args) {
+        int arr[] = {5, 4, 3, 2, 1};
+        
+    }
+}
